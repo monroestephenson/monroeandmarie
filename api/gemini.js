c47965da4fca8cd1721d12e640a4e05b8ceb30b8
monroestephenson <monroestephenson@users.noreply.github.com>
2024-12-10 11:12:59 +0000

    deploy: 417b50d6ef2bc81e68ff0b56024bfbb584a5fb63

--- api/gemini.js
+++ api/gemini.js
@@ -45,7 +45,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/tunedModels/monroechatbot-ql4fhe5lcckn:generateContent?key=${GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: {
